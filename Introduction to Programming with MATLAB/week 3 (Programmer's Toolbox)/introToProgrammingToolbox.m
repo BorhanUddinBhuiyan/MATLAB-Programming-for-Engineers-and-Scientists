dc7e693838ef2a43e78b2201e5fe0423c617da7a
Borhan Uddin Bhuiyan <borhanuddinbhuiyan303@gmail.com>
2021-01-14 15:58:59 -0300

    polymorpism defination

---
+++ Introduction to Programming with MATLAB/week 3 (Programmer's Toolbox)/introToProgrammingToolbox.m
@@ -0,0 +1,11 @@
+% Polymorphism:
+%{
+>if the type of an input argument to afunction can very from one call to
+another, it is called a "polymorphic" function.
+> 'polymorphic' means "having multiple forms" and it can save us huge
+amount fo work 
+> 2nd form of poymorphism: variation in the number of arguments from call
+to call.
+>Many built-in Matlab functions employ the first  from of polymorhism by
+returning and output that is shaped like the intput.
+%}
