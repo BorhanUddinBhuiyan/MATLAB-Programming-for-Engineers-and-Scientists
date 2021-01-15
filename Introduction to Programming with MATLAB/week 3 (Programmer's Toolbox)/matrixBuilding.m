bf077b67684e21842d84e0c9518e1be6ac8a44cc
Borhan Uddin Bhuiyan <borhanuddinbhuiyan303@gmail.com>
2021-01-15 06:50:48 -0300

    matrix building file

---
+++ Introduction to Programming with MATLAB/week 3 (Programmer's Toolbox)/matrixBuilding.m
@@ -0,0 +1,12 @@
+%Random Squence Initialization:
+%{
+> When we start MATLAB and call 'rand', it always returns the same exact
+number: 0.8147 
+> "Pseudo" random number generator: initialized at startupp and it
+generates the exact same sequence of nmbers evey time
+> Repeatability for testing the program while we are developing it : good!
+> what if we want a sequence that is different from default or "truly" rand
+nubmers?
+  > initialize the MATLAB pesudo random number generator with the built-in
+  function "rng"
+%}
